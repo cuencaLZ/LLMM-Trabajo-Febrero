f8821fe79a49c35166f0f7b4ff6f6b3fb4f7614b
Lazarocp <lazaro.cuenca8@gmail.com>
2018-01-21 22:20:18 +0100

    Pagina vision de precios moneda

---
+++ Ver €cripto/Pcripto.js
@@ -0,0 +1,20 @@
+function cambiar_color_over(celda){ 
+   celda.style.color="red";
+   } 
+function cambiar_color_out(celda){ 
+   celda.style.color="";
+   } 
+function Click(celda){
+    for (var k = 1; k < 26; k++){
+        document.getElementById((k).toString()).style.display = "none";
+            }
+        document.getElementById(celda.attributes["class"].value).style.display = "block";
+        }
+var par=false; 
+function parpadeo() { 
+    col=par ? 'red' : 'black'; 
+    document.getElementById('txt').style.color=col; 
+    par = !par; 
+    setTimeout("parpadeo()",500); //500 = medio segundo 
+} 
+window.onload=parpadeo; 
