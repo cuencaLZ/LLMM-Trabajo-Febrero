af3f94af5eea6ca28ec3b7283da6c2e7de15660c
Lazarocp <lazaro.cuenca8@gmail.com>
2018-01-28 17:51:59 +0100

    cambios 9

--- NoticiasCrip/NoticiasC.js
+++ NoticiasCrip/NoticiasC.js
@@ -1,10 +1,14 @@
-function read_more() {
-    document.getElementById('text1_oculto').style.display = 'block';
+function showHide(shID) {
+	if (document.getElementById(shID)) {
+		if (document.getElementById(shID+'-show').style.display != 'none') {
+			document.getElementById(shID+'-show').style.display = 'none';
+			document.getElementById(shID).style.display = 'block';
+		}
+		else {
+			document.getElementById(shID+'-show').style.display = 'inline';
+			document.getElementById(shID).style.display = 'none';
+		}
+	}
 }
 
-document.getElementById('leer_mas1').addEventListener('click', read_more);
- 
 
-
-    
-    
