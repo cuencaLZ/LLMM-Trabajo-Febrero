f1bb3a437f67c8fa17c97b793bb6fec9908f4a4a
Lazarocp <lazaro.cuenca8@gmail.com>
2018-01-31 18:47:25 +0100

    cambios 14

---
+++ HistoriaCripto/Historia.js
@@ -0,0 +1,11 @@
+function cambiar_color_over(celda){ 
+
+    celda.style.backgroundColor="green"
+    celda.style.color= "red"
+    celda.style.fontSize= "20px"
+    } 
+function cambiar_color_out(celda){ 
+   celda.style.backgroundColor=""
+   celda.style.color="";
+   celda.style.fontSize= ""
+   } 
